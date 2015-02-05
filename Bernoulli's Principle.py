4184286c0faeda9b49de95834baccc618070a566
Ryan Joya <ryanjoya@users.noreply.github.com>
2015-02-04 19:24:44 -0600

    Create Bernoulli's Principle.py

---
+++ Bernoulli's Principle.py
@@ -0,0 +1,2 @@
+print "For inviscid flow, an increase in the speed of the fluid occurs simultaneously with a decrease in pressure or a decrease in the fluid's potential energy."
+print "P+rhov^2/2+rhogh = constant"
