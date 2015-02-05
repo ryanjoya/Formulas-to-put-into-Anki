6dc229dd6c819872ba3becd51a949f9f7dbb7172
Ryan Joya <ryanjoya@users.noreply.github.com>
2015-02-04 19:27:42 -0600

    Create Law of Effect.py

---
+++ Law of Effect.py
@@ -0,0 +1,6 @@
+print "noun | (psychology) the principle that behaviors are selected by their consequences; behavior having good consequences tends to be repeated whereas behavior that leads to bad consequences is not repeated"
+print "In an influential paper, R. J. Herrnstein (1970)[8] proposed a quantitative relationship between response rate (B) and reinforcement rate (Rf):
+
+B = k Rf / (Rf0 + Rf)
+
+where k and Rf0 are constants"
